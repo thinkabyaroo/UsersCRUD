555e9f2dde3c34d36f971055661e84c5ab23158e
thinkabyaroo <thinkabyar@isgm2.com>
2023-02-07 10:24:21 +0630

    user edit

--- src/main/java/com/mycompany/user/UserService.java
+++ src/main/java/com/mycompany/user/UserService.java
@@ -3,6 +3,8 @@ package com.mycompany.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired private UserRepository repo;
@@ -12,5 +14,13 @@ public class UserService {
 
     public void save(User user) {
         repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if (result.isPresent()){
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find any users with ID"+ id);
     }
 }
